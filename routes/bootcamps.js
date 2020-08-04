44e9f6ed2e09b77306b217c64c6e91e59104f7d6
sujanth21 <sujanth1987@gmail.com>
2020-08-04 12:52:00 +1000

    controller methods added and refactored routers

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,35 +1,19 @@
 const express = require("express");
 const router = express.Router();
+const {
+  getBootcamps,
+  getBootcamp,
+  createBootcamp,
+  updateBootcamp,
+  deleteBootcamp,
+} = require("../controllers/bootcamps");
 
-//Getting all bootcamps
-router.get("/", (req, res) => {
-  res.status(200).json({ success: true, msg: "Show all bootcamps" });
-});
+router.route("/").get(getBootcamps).post(createBootcamp);
 
-//Getting a specific bootcamp
-router.get("/:id", (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Show bootcamp ${req.params.id}` });
-});
-
-//Creating a new bootcamp
-router.post("/", (req, res) => {
-  res.status(200).json({ success: true, msg: "Create a new bootcamp" });
-});
-
-//Update a specific bootcamp
-router.put("/:id", (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Update bootcamp ${req.params.id}` });
-});
-
-//Delete a specific bootcamp
-router.delete("/:id", (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
-});
+router
+  .route("/:id")
+  .get(getBootcamp)
+  .put(updateBootcamp)
+  .delete(deleteBootcamp);
 
 module.exports = router;
