efb87ee96dc3c677e26e7e4abfe65ddaee120b82
sujanth21 <sujanth1987@gmail.com>
2020-08-04 12:03:45 +1000

    added routes for bootcamps

--- server.js
+++ server.js
@@ -6,6 +6,39 @@ dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
+//Bootcamps route
+//Getting all bootcamps
+app.get("/api/v1/bootcamps", (req, res) => {
+  res.status(200).json({ success: true, msg: "Show all bootcamps" });
+});
+
+//Getting a specific bootcamp
+app.get("/api/v1/bootcamps/:id", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Show bootcamp ${req.params.id}` });
+});
+
+//Creating a new bootcamp
+app.post("/api/v1/bootcamps", (req, res) => {
+  res.status(200).json({ success: true, msg: "Create a new bootcamp" });
+});
+
+//Update a specific bootcamp
+app.put("/api/v1/bootcamps/:id", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Update bootcamp ${req.params.id}` });
+});
+
+//Delete a specific bootcamp
+app.delete("/api/v1/bootcamps/:id", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
+});
+
+//Listening Server
 app.listen(process.env.PORT, () => {
   console.log(
     `Server is running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`
