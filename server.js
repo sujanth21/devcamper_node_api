88a64cad1f9b419ee4f81a9aeafbfb88dfcb7858
sujanth21 <sujanth1987@gmail.com>
2020-08-05 10:05:08 +1000

    Bootcamp model created

--- server.js
+++ server.js
@@ -1,19 +1,37 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
+const colors = require("colors");
+const connectDB = require("./config/db");
 
 const bootcampsRouter = require("./routes/bootcamps");
 
 //Load environment variables
 dotenv.config({ path: "./config/config.env" });
+connectDB();
 
 const app = express();
+
+//Middlewares
+// Dev logging middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
 //Mount routers
 app.use("/api/v1/bootcamps", bootcampsRouter);
 
 //Listening Server
-app.listen(process.env.PORT, () => {
+const server = app.listen(process.env.PORT, () => {
   console.log(
     `Server is running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`
+      .yellow.bold
   );
+});
+
+//Handled unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  //Close server and exit process
+  server.close(() => process.exit(1));
 });
